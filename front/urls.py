a9338ce4693509f8600d98775d5045d8aca6760f
Margarita <mv@asofts.ru>
2020-09-04 16:07:58 +0700

    registration fix login finished logout finished

--- front/urls.py
+++ front/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('me', views.Account.as_view()),
     path('register', views.Register.as_view()),
     path('login', views.Login.as_view()),
+    path('logout', views.Logout.as_view()),
 ]
 
--- front/views.py
+++ front/views.py
@@ -4,6 +4,7 @@ import random
 import environ
 from os import path
 
+from django.contrib.auth import authenticate, login, logout
 from django.core.mail import send_mail
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -27,44 +28,56 @@ base_dir = settings.BASE_DIR
 env = environ.Env()
 env.read_env(path.join(base_dir, '.env'))
 
+class ContextViewMixin(View):
+    def make_context(self, context=None, **kwargs):
+        if not context: context = {}
+        if self.request.user.is_authenticated:
+            context['user'] = self.request.user
+        else:
+            context['user'] = None
+        # try:
+        #     settings = models.Settings.objects.get()
+        # except:
+        #     context = {}
+        # else:
+        #     context = {'settings': settings}
+        if kwargs:
+            for k, v in kwargs.items():
+                context[f'{k}'] = v
+        return context
 
-def make_context(context=None, **kwargs):
-    if not context: context = {}
-    # try:
-    #     settings = models.Settings.objects.get()
-    # except:
-    #     context = {}
-    # else:
-    #     context = {'settings': settings}
-    if kwargs:
-        for k, v in kwargs.items():
-            context[f'{k}'] = v
-    return context
-
-
-class Index(View):
+#todo feedback form for questions
+class Index(ContextViewMixin):
     def get(self, request):
-        context = make_context()
+        context = self.make_context()
         return render(request, 'index.html', context=context)
 
 
 LETTERS = ascii_lowercase
 NUMBERS = ''.join(str(i) for i in range(10))
 
-
+# todo refactor to functions file
 def generate_code(length=16):
     bool_range = range(2)
     code = ''.join(map(lambda x: random.choice(NUMBERS) if random.choice(bool_range) else str.upper(
         random.choice(LETTERS)) if random.choice(bool_range) else random.choice(LETTERS), range(length)))
     return code
 
-
+#todo template of registry
+# в шаблоне сделать кнопку для перехода на страницу входа
+# и кнопку для пометки ошибочной регистрации (по нажатию запрос на api и удаление из базы этого пользователя по почте)
 @method_decorator(csrf_exempt, name='dispatch')
-class Register(View):
+class Register(ContextViewMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return super(Register, self).dispatch(request)
+        else:
+            return HttpResponseRedirect('/me')
+
     def get(self, request, form=None):
         if form is None: form = forms.RegisterAccount()
         form_html = get_template('includes/form_register.html').render(context={"form": form}, request=request)
-        context = make_context(form_html=form_html)
+        context = self.make_context(form_html=form_html)
         return render(request, 'auth.html', context=context)
 
     def post(self, request):
@@ -86,7 +99,7 @@ class Register(View):
                     user.last_name = lastname
                     user.email = email
                     password = generate_code(length=8)
-                    user.password = password
+                    user.set_password(password)
                     user.save()
                     account = models.Account.objects.create(user=user, phone=phone)
 
@@ -112,12 +125,12 @@ class Register(View):
                             return HttpResponseRedirect('/login')
         return self.get(request, form=form)
 
-
+#todo recovery password with email template
 @method_decorator(csrf_exempt, name='dispatch')
-class Login(View):
+class Login(ContextViewMixin):
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
-            return super(Login, self).dispatch(request)
+            return super().dispatch(request, *args, **kwargs)
         else:
             return HttpResponseRedirect('/me')
 
@@ -126,14 +139,32 @@ class Login(View):
         registry = request.session.pop('registry') if 'registry' in request.session else None
         context_insert = {"form": form, "registry": registry}
         form_html = get_template('includes/form_login.html').render(context=context_insert, request=request)
-        context = make_context(form_html=form_html)
+        context = self.make_context(form_html=form_html)
         return render(request, 'auth.html', context=context)
 
+    def post(self, request):
+        form = forms.Login(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("login")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user:
+                login(self.request, user)
+                return HttpResponseRedirect('/me')
+            else:
+                form.errors['custom'] = f"Неверный логин или пароль"
+        return self.get(request, form=form)
 
+class Logout(View):
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return HttpResponseRedirect("/")
 
-# class Account(LoginRequiredMixin, View):
-class Account(View):
+#todo feedback
+class Account(LoginRequiredMixin, ContextViewMixin):
+# class Account(View):
     def get(self, request):
         # redirect_field_name = 'redirect_to'
-        context = make_context(tmp='qwe qweqwe qwe')
+        context = self.make_context(tmp='qwe qweqwe qwe')
         return render(request, 'account_ready.html', context=context)
