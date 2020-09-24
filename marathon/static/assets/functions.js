e98edafa4352a9d844445fd5b02586d22f895c61
Margarita <mv@asofts.ru>
2020-09-24 10:06:27 +0700

    js fix

--- marathon/static/assets/functions.js
+++ marathon/static/assets/functions.js
@@ -29,3 +29,18 @@ function valid_contact(id) {
     field = document.getElementById(id);
     field.value = field.value.replace(/[^0-9aA-zZаА-яЯ)( @.+-]+/g, '');
 }
+
+function bio(mode){
+        var p = document.getElementById('full_bio');
+        if (mode==1){
+            document.getElementById('more-bio').innerText = '';
+             p.innerHTML = '<a class="text-primary" onclick="bio(0)">Показать меньше</a>';
+        $("[name='slide_block']").slideDown( "slow" );
+        }
+        else if (mode==0) {
+    		p.innerText = '';
+    		$("[name='slide_block']").slideUp( "slow" );
+		document.getElementById('more-bio').innerHTML = 'Показать больше...&nbsp;';
+}
+        else return;
+    }
